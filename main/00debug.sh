e7d5a38ec0be6a60eaabda3fe2bf56f815d62060
Lerham <limrim2.3.26@gmail.com>
2026-05-10 05:43:48 +0900

    part2 校正

--- main/00debug.sh
+++ main/00debug.sh
@@ -10,33 +10,44 @@ get_qgroup() {
     printf "%03d-%03d" ${s} ${e}
 }
 
-get_problem_dir() {
+get_q2group() {
+    q2=$((10#$1))
+    s=$(( ((q2 - 1) / 20) * 20 + 1 ))
+    e=$(( s + 19 ))
+    printf "%03d-%03d" ${s} ${e}
+}
+
+get_part2_qnumber() {
     q=$((10#$1))
     case ${q} in
-        250) echo "../problems/part_2/Q_2_6" ;;
-        256) echo "../problems/part_2/Q_2_4" ;;
-        259) echo "../problems/part_2/Q_2_5" ;;
-        266) echo "../problems/part_2/Q_2_3" ;;
-        267) echo "../problems/part_2/Q_2_2" ;;
-        268) echo "../problems/part_2/Q_2_1" ;;
-        *)
-            qgroup=`get_qgroup $1`
-            echo "../problems/part_1/${qgroup}/Q_$1"
-            ;;
+        250) echo "6" ;;
+        256) echo "4" ;;
+        259) echo "5" ;;
+        266) echo "3" ;;
+        267) echo "2" ;;
+        268) echo "1" ;;
+        *) echo "" ;;
     esac
 }
 
+get_problem_dir() {
+    part2_q=`get_part2_qnumber $1`
+    if [ -n "${part2_q}" ] ; then
+        q2group=`get_q2group ${part2_q}`
+        echo "../problems/part_2/${q2group}/Q_2_${part2_q}"
+    else
+        qgroup=`get_qgroup $1`
+        echo "../problems/part_1/${qgroup}/Q_$1"
+    fi
+}
+
 get_problem_base() {
-    q=$((10#$1))
-    case ${q} in
-        250) echo "Q_2_6" ;;
-        256) echo "Q_2_4" ;;
-        259) echo "Q_2_5" ;;
-        266) echo "Q_2_3" ;;
-        267) echo "Q_2_2" ;;
-        268) echo "Q_2_1" ;;
-        *) echo "Q_$1" ;;
-    esac
+    part2_q=`get_part2_qnumber $1`
+    if [ -n "${part2_q}" ] ; then
+        echo "Q_2_${part2_q}"
+    else
+        echo "Q_$1"
+    fi
 }
 
 default_q=$(sed -n 's/^\\providecommand{\\SMDefaultDebugProblemNumber}{\([0-9][0-9]*\)}$/\1/p' ../config/config.tex | tr -d '\r' | tail -n 1)
